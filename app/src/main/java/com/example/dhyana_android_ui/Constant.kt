58d6575adfae1971aa85c49e5da6ae9051de3494
Dhyana Dave <dhyana.d@simformsolutions.com>
2022-05-09 18:25:50 +0530

    Splash screen done

---
+++ app/src/main/java/com/example/dhyana_android_ui/Constant.kt
@@ -0,0 +1,5 @@
+package com.example.dhyana_android_ui
+
+object Constant {
+    const val THREE_H = 3000L
+}
---
+++ app/src/main/java/com/example/dhyana_android_ui/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package com.example.dhyana_android_ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            finish()
+        }, Constant.THREE_H)
+    }
+}
