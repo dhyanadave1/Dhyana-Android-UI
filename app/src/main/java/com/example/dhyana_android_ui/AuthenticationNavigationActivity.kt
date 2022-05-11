bd7955af12e6e2f21e2e9529147e62176ce96e80
Dhyana Dave <dhyana.d@simformsolutions.com>
2022-05-11 12:43:21 +0530

    Authentication navigation screen done

--- app/src/main/java/com/example/dhyana_android_ui/AuthenticationNavigationActivity.kt
+++ app/src/main/java/com/example/dhyana_android_ui/AuthenticationNavigationActivity.kt
@@ -3,9 +3,9 @@ package com.example.dhyana_android_ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AuthenticationNavigationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_authentication_navigation)
     }
 }
